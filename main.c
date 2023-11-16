1d5ce1b9d52618f5778bfe90c976f79d56e2e8a6
Juan Bindez <juanbindez780@gmail.com>
2023-11-15 22:46:37 -0300

    Create main.c

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define MAX_LENGTH 1000
+
+// Função para executar comandos do Git a partir do arquivo de configuração
+void executeGitCommands(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        perror("Erro ao abrir o arquivo de configuração");
+        exit(EXIT_FAILURE);
+    }
+
+    char command[MAX_LENGTH];
+    while (fgets(command, sizeof(command), file) != NULL) {
+        // Remover a quebra de linha no final do comando
+        char *newline = strchr(command, '\n');
+        if (newline != NULL) {
+            *newline = '\0';
+        }
+
+        // Executar o comando Git
+        system(command);
+    }
+
+    fclose(file);
+}
+
+int main() {
+    // Nome do arquivo de configuração
+    const char *configFileName = "makegit";
+
+    // Executar os comandos do Git a partir do arquivo de configuração
+    executeGitCommands(configFileName);
+
+    return 0;
+}
