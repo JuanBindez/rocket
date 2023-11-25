1368c474d061a5fd7d1f6c521f20bfd241ae28c4
Juan Bindez <juanbindez780@gmail.com>
2023-11-25 13:59:15 -0300

    Update help.c

--- src/help.c
+++ src/help.c
@@ -2,19 +2,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define VERSION "1.0.0"
+#define VERSION "1.1.0"
 #define AUTHOR "Copyright (C) 2023 Juan Bindez <juanbindez780@gmail.com>"
 #define LICENSE "GPLv2 License"
 
 void showHelp() {
-    printf("Makeline %s\n\n", VERSION);
-    printf("%s\n", AUTHOR);
-    printf("%s\n\n", LICENSE);
+    printf("Makeline %s\n", VERSION);
     printf("Usage: makeline [-h]\n");
-    printf("Executes commands from a configuration file.\n\n");
+    printf("Executes commands from a configuration file.\n");
     printf("Options:\n");
     printf("  -h\t\tDisplay this help message.\n");
     printf("  -v\t\tDisplays the program version.\n");
+    printf("%s\n", AUTHOR);
+    printf("%s\n\n", LICENSE);
 }
 
 void showVersion() {
